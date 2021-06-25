63356b09461176fd95f90245216d058b748248c8
Pablo Vidal Otero <pablo.vidal@almis.com>
2021-06-25 12:21:40 +0000

    Add redis as database cache for persist user sessions

--- awe-tests/awe-boot/src/main/java/com/almis/awe/config/SpecificSecurityConfig.java
+++ awe-tests/awe-boot/src/main/java/com/almis/awe/config/SpecificSecurityConfig.java
@@ -21,7 +21,8 @@ public class SpecificSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.antMatcher(actuatorEndpoint + "/**").anonymous();
+    http.antMatcher(actuatorEndpoint + "/**").anonymous()
+    .and().csrf().disable();
   }
 
   @Bean
