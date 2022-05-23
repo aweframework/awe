e485b1bf7b826189d1c997972f00507c0e5d4889
Pablo Vidal Otero <pablo.vidal@almis.com>
2022-05-23 12:28:53 +0000

    Resolve "Error in sqlserver flyway scripts"

--- awe-framework/awe-starters/awe-spring-boot-starter/src/main/resources/db/migration/sqlserver/AWE_V1.0.1__2fa_data.sql
+++ awe-framework/awe-starters/awe-spring-boot-starter/src/main/resources/db/migration/sqlserver/AWE_V1.0.1__2fa_data.sql
@@ -1,8 +1,8 @@
 --------------------------------------------------------
 --  ADD enable2fa and secret2fa columns to ope table
 --------------------------------------------------------
-ALTER TABLE ope ADD COLUMN enable2fa INT DEFAULT 0 NOT NULL;
-ALTER TABLE ope ADD COLUMN secret2fa VARCHAR(128) NULL;
+ALTER TABLE ope ADD enable2fa INT DEFAULT 0 NOT NULL;   --- Enable 2fa
+ALTER TABLE ope ADD secret2fa VARCHAR(128) NULL;        --- Secret 2fa
 
-ALTER TABLE HISope ADD COLUMN enable2fa INT NULL;
-ALTER TABLE HISope ADD COLUMN secret2fa VARCHAR(128) NULL;
+ALTER TABLE HISope ADD enable2fa INT NULL;
+ALTER TABLE HISope ADD secret2fa VARCHAR(128) NULL;
