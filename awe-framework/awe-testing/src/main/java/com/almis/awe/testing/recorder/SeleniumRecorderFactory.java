5500f5ed781c39160541ee3d9c185ed9936a29e0
pablo.garcia.almis <pablo.garcia@almis.com>
2022-06-02 16:31:49 +0200

    Allow to use remote recorders if defined

--- awe-framework/awe-testing/src/main/java/com/almis/awe/testing/recorder/SeleniumRecorderFactory.java
+++ awe-framework/awe-testing/src/main/java/com/almis/awe/testing/recorder/SeleniumRecorderFactory.java
@@ -9,16 +9,17 @@ import com.automation.remarks.video.recorder.monte.MonteRecorder;
 import org.apache.commons.lang3.SystemUtils;
 
 public class SeleniumRecorderFactory {
+  private SeleniumRecorderFactory() {}
   public static IVideoRecorder getRecorder(RecorderType recorderType) {
     if (RecorderType.FFMPEG.equals(recorderType)) {
-      if (SystemUtils.IS_OS_WINDOWS) {
-        return new WindowsFFmpegRecorder();
-      } else if (SystemUtils.IS_OS_MAC) {
-        return new MacFFmpegRecorder();
-      } else if ("browser".equals(System.getProperty("isDocker"))) {
+      if ("browser".equals(System.getProperty("isDocker"))) {
         return new DockerFFMpegRecorder();
       } else if ("browserRecorder".equals(System.getProperty("isDocker"))) {
         return new DockerServiceFFMpegRecorder();
+      } else if (SystemUtils.IS_OS_WINDOWS) {
+        return new WindowsFFmpegRecorder();
+      } else if (SystemUtils.IS_OS_MAC) {
+        return new MacFFmpegRecorder();
       } else {
         return new LinuxFFmpegRecorder();
       }
