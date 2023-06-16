a5204a60745f93131088db959ce3a1c050b35aae
pablo.garcia.almis <pablo.garcia@almis.com>
2023-06-16 08:05:01 +0200

    Removed @Component on RoutingDatasourceInitializer

--- awe-framework/awe-controller/src/main/java/com/almis/awe/component/RoutingDatasourceInitializer.java
+++ awe-framework/awe-controller/src/main/java/com/almis/awe/component/RoutingDatasourceInitializer.java
@@ -1,14 +1,11 @@
 package com.almis.awe.component;
 
-import org.springframework.stereotype.Component;
-
 import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
 /**
  * Component used for load datasource target map in AweRoutingDataSource {@link AweRoutingDataSource}
  */
-@Component
 public class RoutingDatasourceInitializer
 {
     private final DataSource dataSource;
