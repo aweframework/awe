b96d9289ae2aa48f91267b32733475c14d463f5f
pablo.garcia.almis <pablo.garcia@almis.com>
2023-11-17 09:54:14 +0100

    Fix web security config

--- awe-framework/awe-controller/src/test/java/com/almis/awe/security/authentication/filter/PublicQueryMaintainAuthorizationTest.java
+++ awe-framework/awe-controller/src/test/java/com/almis/awe/security/authentication/filter/PublicQueryMaintainAuthorizationTest.java
@@ -5,8 +5,9 @@ import com.almis.awe.model.component.AweElements;
 import com.almis.awe.model.entities.maintain.Target;
 import com.almis.awe.model.entities.queries.Query;
 import com.almis.awe.security.authorization.PublicQueryMaintainAuthorization;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -25,50 +26,113 @@ class PublicQueryMaintainAuthorizationTest {
   @Mock
   AweElements elements;
 
-  @Test
-  void givenNullAweElement_checkIsPublicQuery_shouldReturnTrue() throws AWException {
+  @ParameterizedTest
+  @ValueSource(strings = {
+    "/action/data",
+    "/action/update-value",
+    "/action/data-silent",
+    "/action/unique",
+    "/action/control",
+    "/action/control-cancel",
+    "/action/validate",
+    "/action/subscribe",
+    "/action/tree-branch",
+    "/api/data"
+  })
+  void givenNullAweElement_checkIsPublicQuery_shouldReturnTrue(String action) throws AWException {
     MockHttpServletRequest request = new MockHttpServletRequest();
-    request.setRequestURI("/action/data/foo");
+    request.setRequestURI(action + "/foo");
     when(elements.getQuery(anyString())).thenReturn(new Query().setIsPublic(true));
     assertTrue(publicQueryMaintainAuthorization.isPublicQuery(request));
   }
 
-  @Test
-  void givenNullAweElement_checkIsPublicQuery_shouldReturnFalse() throws AWException {
+  @ParameterizedTest
+  @ValueSource(strings = {
+    "/action/data",
+    "/action/update-value",
+    "/action/data-silent",
+    "/action/unique",
+    "/action/control",
+    "/action/control-cancel",
+    "/action/validate",
+    "/action/subscribe",
+    "/action/tree-branch",
+    "/api/data"
+  })
+  void givenNullAweElement_checkIsPublicQuery_shouldReturnFalse(String action) throws AWException {
     MockHttpServletRequest request = new MockHttpServletRequest();
-    request.setRequestURI("/action/data/foo");
+    request.setRequestURI(action + "/foo");
     when(elements.getQuery(anyString())).thenReturn(new Query());
     assertFalse(publicQueryMaintainAuthorization.isPublicQuery(request));
   }
 
-  @Test
-  void givenNullAweElement_checkIsPublicQuery_shouldReturnNull() throws AWException {
+  @ParameterizedTest
+  @ValueSource(strings = {
+    "/action/data",
+    "/action/update-value",
+    "/action/data-silent",
+    "/action/unique",
+    "/action/control",
+    "/action/control-cancel",
+    "/action/validate",
+    "/action/subscribe",
+    "/action/tree-branch",
+    "/api/data"
+  })
+  void givenNullAweElement_checkIsPublicQuery_shouldReturnNull(String action) throws AWException {
     MockHttpServletRequest request = new MockHttpServletRequest();
-    request.setRequestURI("/action/data/foo");
+    request.setRequestURI(action + "/foo");
     when(elements.getQuery(anyString())).thenReturn(null);
     assertThrows(AuthorizationServiceException.class, () -> publicQueryMaintainAuthorization.isPublicQuery(request));
   }
 
-  @Test
-  void givenNullAweElement_checkIsPublicMaintain_shouldReturnTrue() throws AWException {
+  @ParameterizedTest
+  @ValueSource(strings = {
+    "/action/maintain",
+    "/action/maintain-silent",
+    "/action/get-file-maintain",
+    "/file/stream/maintain",
+    "/file/download/maintain",
+    "/api/maintain",
+    "/api/public/maintain"
+  })
+  void givenNullAweElement_checkIsPublicMaintain_shouldReturnTrue(String action) throws AWException {
     MockHttpServletRequest request = new MockHttpServletRequest();
-    request.setRequestURI("/action/maintain/foo");
+    request.setRequestURI(action + "/foo");
     when(elements.getMaintain(anyString())).thenReturn(new Target().setIsPublic(true));
     assertTrue(publicQueryMaintainAuthorization.isPublicMaintain(request));
   }
 
-  @Test
-  void givenNullAweElement_checkIsPublicMaintain_shouldReturnFalse() throws AWException {
+  @ParameterizedTest
+  @ValueSource(strings = {
+    "/action/maintain",
+    "/action/maintain-silent",
+    "/action/get-file-maintain",
+    "/file/stream/maintain",
+    "/file/download/maintain",
+    "/api/maintain",
+    "/api/public/maintain"
+  })
+  void givenNullAweElement_checkIsPublicMaintain_shouldReturnFalse(String action) throws AWException {
     MockHttpServletRequest request = new MockHttpServletRequest();
-    request.setRequestURI("/action/maintain/foo");
+    request.setRequestURI(action + "/foo");
     when(elements.getMaintain(anyString())).thenReturn(new Target());
     assertFalse(publicQueryMaintainAuthorization.isPublicMaintain(request));
   }
 
-  @Test
-  void givenNullAweElement_checkIsPublicMaintain_shouldReturnNull() throws AWException {
+  @ParameterizedTest
+  @ValueSource(strings = {
+    "/action/maintain",
+    "/action/maintain-silent",
+    "/action/get-file-maintain",
+    "/file/stream/maintain",
+    "/file/download/maintain",
+    "/api/maintain",
+    "/api/public/maintain"
+  })
+  void givenNullAweElement_checkIsPublicMaintain_shouldReturnNull(String action) throws AWException {
     MockHttpServletRequest request = new MockHttpServletRequest();
-    request.setRequestURI("/action/maintain/foo");
+    request.setRequestURI(action + "/foo");
     when(elements.getMaintain(anyString())).thenReturn(null);
     assertThrows(AuthorizationServiceException.class, () -> publicQueryMaintainAuthorization.isPublicMaintain(request));
   }
