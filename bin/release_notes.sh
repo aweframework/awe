588586a9f2c7800ac648ba383b51badbae937714
pablo.garcia.almis <pablo.garcia@almis.com>
2024-05-29 08:28:59 +0200

    Added gitlab release

---
+++ bin/release_notes.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Get merge requests and transforms into releaseNotes
+echo "Generating release notes for version ${4}"
+currentDate=$(date +%d/%m/%Y)
+releaseNotes=$(curl --header "Authorization: Bearer ${2}" -s "${1}/merge_requests?milestone=${4}&state=merged&per_page=200" | jq --raw-output 'map("- " + (if (.labels | map(select(. == "has impacts")) | length == 1) then "**[HAS IMPACTS]** " else "" end) + (.title | gsub("Resolve ";"") | gsub("\"";"")) + ". [MR #" + (.iid | tostring) + "](" + .web_url + ") (" + .merged_by.name + ")") | join("\n")')
+releaseNotesText=$(printf "\n# Release notes for ${3} ${4}\n*${currentDate}*\n\n${releaseNotes}\n\n")
+echo "$releaseNotesText"
+
+# Generate release notes
+echo "$releaseNotesText" > ./release_notes.md && echo "Generated release notes file at ./release_notes.md"
