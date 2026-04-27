4912b28a12c5a717e97429c16e4708ffc0843a74
Pablo Vidal Otero <pablo.vidal@almis.com>
2026-04-27 13:25:15 +0200

    [HAS IMPACTS] build(frontend): migrate frontend builds to npm

--- awe-tests/awe-scheduler-boot/webpack.config.js
+++ awe-tests/awe-scheduler-boot/webpack.config.js
@@ -32,22 +32,19 @@ module.exports = {
     rules : [
       { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/},
       // Hack to load angular synchronously
-      { test : /[\/]angular\.js$/, loader : "exports-loader?angular"},
+      { test : /[\/]angular\.js$/, loader : "exports-loader", options: { exports: "angular" }},
       { test : /\.css$/, include : [ styleDir ], use : [MiniCssExtractPlugin.loader, "css-loader"]},
       { test : /\.less$/, include : [ styleDir ], use : [MiniCssExtractPlugin.loader, "css-loader", {
-          loader: "less-loader", options: { lessPlugins: [ new LessPluginAutoPrefix({browsers: autoprefixerBrowsers}) ],
-            minimize: true, sourceMap: true}}]},
-      { test : /\.(jpg|gif|png)$/, loader : 'url-loader?limit=100000&name=./images/[hash].[ext]'},
-      { test : /\.woff[2]*?(\?v=[0-9]\.[0-9]\.[0-9])?$/, use : "url-loader?limit=10000&mimetype=application/font-woff&name=./fonts/[hash].[ext]"},
-      { test : /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, use : "file-loader?name=./fonts/[hash].[ext]"}
+          loader: "less-loader", options: { lessOptions: { plugins: [ new LessPluginAutoPrefix({browsers: autoprefixerBrowsers}) ] }, sourceMap: false }}]},
+      { test : /\.(jpg|gif|png)$/, type: 'asset', parser: { dataUrlCondition: { maxSize: 100000 } }, generator: { filename: './images/[hash][ext][query]' }},
+      { test : /\.woff[2]*?(\?v=[0-9]\.[0-9]\.[0-9])?$/, type: 'asset', parser: { dataUrlCondition: { maxSize: 10000 } }, generator: { filename: './fonts/[hash][ext][query]', dataUrl: content => `data:application/font-woff;base64,${content.toString('base64')}` }},
+      { test : /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, type: 'asset/resource', generator: { filename: './fonts/[hash][ext][query]' }}
     ]
   },
   resolve : {
     extensions : [ ".js", ".css", ".less", "*" ]
   },
   plugins : [ new MiniCssExtractPlugin({
-    filename: "css/specific.css",
-    disable: false,
-    allChunks: true
+    filename: "css/specific.css"
   })]
 };
