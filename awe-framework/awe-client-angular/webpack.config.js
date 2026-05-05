0774c2c196c645aca32b57c4d0ea888c0fa1ead2
Pablo Vidal Otero <pablo.vidal@almis.com>
2026-05-05 21:46:58 +0200

    [HAS IMPACTS] Align frontend build modes for Webpack modules

--- awe-framework/awe-client-angular/webpack.config.js
+++ awe-framework/awe-client-angular/webpack.config.js
@@ -10,66 +10,87 @@ const srcPath = path.resolve(__dirname, "src", "main", "resources", "js", "awe")
 const libPath = path.resolve(__dirname, "src", "main", "resources", "js", "lib");
 
 const autoprefixerBrowsers = ['last 2 versions', '> 1%', 'opera 12.1', 'bb 10', 'android 4', 'IE 10'];
+const validModes = new Set(["development", "production"]);
 
-module.exports = {
-  mode: process.env.NODE_ENV,
-  devtool: "source-map",
-  entry: {
-    "bundle": path.join(dir, "awe.config.js"),
-    "locals-es-ES": path.join(dir, "locals-es-ES.config.js"),
-    "locals-en-GB": path.join(dir, "locals-en-GB.config.js"),
-    "locals-eu-ES": path.join(dir, "locals-eu-ES.config.js"),
-    "locals-fr-FR": path.join(dir, "locals-fr-FR.config.js")
-  },
-  output: {
-    filename: "js/[name].js",
-    path: path.join(__dirname, 'target', 'classes', 'static'),
-    publicPath: "../"
-  },
-  optimization: {
-    splitChunks: {
-      name: "bundle",
-      cacheGroups: {
-        commons: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'commons',
-          chunks: 'all'
+const resolveMode = (env = {}, argv = {}) => {
+  const cliMode = argv.mode;
+  const envMode = env.NODE_ENV || env.mode || process.env.NODE_ENV;
+
+  if (validModes.has(cliMode)) {
+    return cliMode;
+  }
+
+  if (validModes.has(envMode)) {
+    return envMode;
+  }
+
+  return "production";
+};
+
+module.exports = (env = {}, argv = {}) => {
+  const mode = resolveMode(env, argv);
+  const isProduction = mode === "production";
+
+  return {
+    mode,
+    devtool: isProduction ? false : "source-map",
+    entry: {
+      "bundle": path.join(dir, "awe.config.js"),
+      "locals-es-ES": path.join(dir, "locals-es-ES.config.js"),
+      "locals-en-GB": path.join(dir, "locals-en-GB.config.js"),
+      "locals-eu-ES": path.join(dir, "locals-eu-ES.config.js"),
+      "locals-fr-FR": path.join(dir, "locals-fr-FR.config.js")
+    },
+    output: {
+      filename: "js/[name].js",
+      path: path.join(__dirname, 'target', 'classes', 'static'),
+      publicPath: "../"
+    },
+    optimization: {
+      splitChunks: {
+        name: "bundle",
+        cacheGroups: {
+          commons: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'commons',
+            chunks: 'all'
+          }
         }
       }
-    }
-  },
-  module : {
-    rules : [
-      { test: require.resolve('jquery'), use: [{loader: 'expose-loader', options: {exposes: ['jQuery', '$']}}]},
-      { test: /\.jsx?$/, exclude: /node_modules/, use: [{loader: 'babel-loader'}]},
-      { test : /\.css$/, include : [ styleDir ], use : [MiniCssExtractPlugin.loader, "css-loader"]},
-      { test : /\.less$/, include : [ styleDir ], use : [MiniCssExtractPlugin.loader, "css-loader", {
-          loader: "less-loader", options: { lessOptions: { plugins: [ new LessPluginAutoPrefix({browsers: autoprefixerBrowsers}) ] }, sourceMap: false }}]},
-      { test : /\.(jpg|gif|png)$/, type: 'asset', parser: { dataUrlCondition: { maxSize: 100000 } }, generator: { filename: './images/[hash][ext][query]' }},
-      { test : /\.woff[2]*?(\?v=\d+\.\d+\.\d+)?$/, type: 'asset', parser: { dataUrlCondition: { maxSize: 100000 } }, generator: { filename: './fonts/[hash][ext][query]', dataUrl: content => `data:application/font-woff;base64,${content.toString('base64')}` }},
-      { test : /\.(ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/, type: 'asset/resource', generator: { filename: './fonts/[hash][ext][query]' }},
-    ]
-  },
-  resolve: {
-    extensions: [".js", ".css", ".less", "*"],
-    alias: {
-      "jquery": path.resolve(__dirname, "node_modules", "jquery", "dist", "jquery"),
-      "bootstrap-tabdrop": path.resolve(libPath, "bootstrap-tabdrop", "src", "js", "bootstrap-tabdrop"),
-      "HighchartsLocale": path.resolve(libPath, "highcharts", "i18n", "highcharts-lang"),
-      "HighchartsThemes": path.resolve(libPath, "highcharts", "themes", "all"),
-      "Tocify": path.resolve(libPath, "tocify", "jquery.tocify"),
-      "PivotTable": path.resolve(libPath, "pivotTable", "pivotTable")
     },
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "css/styles.css"
-    }),
-    new LodashModuleReplacementPlugin,
-    new webpack.ProvidePlugin({
-      "Highcharts": "highcharts/highstock",
-      "HighchartsLocale": "HighchartsLocale",
-      "window.constructor": "constructor"
-    })
-  ]
+    module : {
+      rules : [
+        { test: require.resolve('jquery'), use: [{loader: 'expose-loader', options: {exposes: ['jQuery', '$']}}]},
+        { test: /\.jsx?$/, exclude: /node_modules/, use: [{loader: 'babel-loader', options: { envName: mode }}]},
+        { test : /\.css$/, include : [ styleDir ], use : [MiniCssExtractPlugin.loader, "css-loader"]},
+        { test : /\.less$/, include : [ styleDir ], use : [MiniCssExtractPlugin.loader, "css-loader", {
+            loader: "less-loader", options: { lessOptions: { plugins: [ new LessPluginAutoPrefix({browsers: autoprefixerBrowsers}) ] }, sourceMap: false }}]},
+        { test : /\.(jpg|gif|png)$/, type: 'asset', parser: { dataUrlCondition: { maxSize: 100000 } }, generator: { filename: './images/[hash][ext][query]' }},
+        { test : /\.woff[2]*?(\?v=\d+\.\d+\.\d+)?$/, type: 'asset', parser: { dataUrlCondition: { maxSize: 100000 } }, generator: { filename: './fonts/[hash][ext][query]', dataUrl: content => `data:application/font-woff;base64,${content.toString('base64')}` }},
+        { test : /\.(ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/, type: 'asset/resource', generator: { filename: './fonts/[hash][ext][query]' }},
+      ]
+    },
+    resolve: {
+      extensions: [".js", ".css", ".less", "*"],
+      alias: {
+        "jquery": path.resolve(__dirname, "node_modules", "jquery", "dist", "jquery"),
+        "bootstrap-tabdrop": path.resolve(libPath, "bootstrap-tabdrop", "src", "js", "bootstrap-tabdrop"),
+        "HighchartsLocale": path.resolve(libPath, "highcharts", "i18n", "highcharts-lang"),
+        "HighchartsThemes": path.resolve(libPath, "highcharts", "themes", "all"),
+        "Tocify": path.resolve(libPath, "tocify", "jquery.tocify"),
+        "PivotTable": path.resolve(libPath, "pivotTable", "pivotTable")
+      },
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "css/styles.css"
+      }),
+      new LodashModuleReplacementPlugin,
+      new webpack.ProvidePlugin({
+        "Highcharts": "highcharts/highstock",
+        "HighchartsLocale": "HighchartsLocale",
+        "window.constructor": "constructor"
+      })
+    ]
+  };
 };
